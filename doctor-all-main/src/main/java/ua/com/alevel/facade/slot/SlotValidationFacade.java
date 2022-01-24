d32dcc5cc9dd5dd20fa41ba2a67c9b2bb92a385a
Liliia <liliia.pohorielova@nure.ua>
2022-01-24 02:30:19 +0200

    Slot Past Validation Add

--- doctor-all-main/src/main/java/ua/com/alevel/facade/slot/SlotValidationFacade.java
+++ doctor-all-main/src/main/java/ua/com/alevel/facade/slot/SlotValidationFacade.java
@@ -5,4 +5,6 @@ import ua.com.alevel.web.dto.request.slot.SlotRequestDto;
 public interface SlotValidationFacade {
 
     String validateSlot(SlotRequestDto slotRequestDto);
+
+    String validatePastSlot(Long slotId);
 }
--- doctor-all-main/src/main/java/ua/com/alevel/facade/slot/impl/SlotValidationFacadeImpl.java
+++ doctor-all-main/src/main/java/ua/com/alevel/facade/slot/impl/SlotValidationFacadeImpl.java
@@ -2,8 +2,14 @@ package ua.com.alevel.facade.slot.impl;
 
 import org.springframework.stereotype.Service;
 import ua.com.alevel.facade.slot.SlotValidationFacade;
+import ua.com.alevel.persistence.entity.slot.Slot;
 import ua.com.alevel.persistence.repository.doctor.SlotRepository;
+import ua.com.alevel.persistence.type.SlotStatus;
 import ua.com.alevel.web.dto.request.slot.SlotRequestDto;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 
 @Service
 public class SlotValidationFacadeImpl implements SlotValidationFacade {
@@ -16,8 +22,18 @@ public class SlotValidationFacadeImpl implements SlotValidationFacade {
 
     public String validateSlot(SlotRequestDto slotRequestDto) {
         String message = "";
-        if (slotRepository.findByDoctorAndAppDateAndStartTime(slotRequestDto.getDoctor(), slotRequestDto.getAppDate(), slotRequestDto.getStartTime()) != null)
+        Slot slot = slotRepository.findByDoctorAndAppDateAndStartTime(slotRequestDto.getDoctor(), slotRequestDto.getAppDate(), slotRequestDto.getStartTime());
+        if (slot != null && slot.getStatus() != SlotStatus.CANCELLED)
             message = "Such slot is already exist!!! Choose another time or date!";
+        return message;
+    }
+
+    @Override
+    public String validatePastSlot(Long slotId) {
+        String message = "";
+        Slot slot = slotRepository.findById(slotId).get();
+        if (slot.getAppDate().isAfter(LocalDate.now()))
+            message = "You can't mark this visit as past. The past hasn't arrived yet :)";
         return message;
     }
 }
