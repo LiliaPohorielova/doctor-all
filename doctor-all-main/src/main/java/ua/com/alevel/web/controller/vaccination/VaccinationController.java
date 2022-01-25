76dc0aa7cb4d4881c18e3d971feb78764984e512
Liliia <liliia.pohorielova@nure.ua>
2022-01-26 00:48:49 +0200

    Get Vaccinations From Supplier

--- doctor-all-main/src/main/java/ua/com/alevel/web/controller/vaccination/VaccinationController.java
+++ doctor-all-main/src/main/java/ua/com/alevel/web/controller/vaccination/VaccinationController.java
@@ -4,13 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.com.alevel.persistence.entity.vaccination.Vaccination;
+import ua.com.alevel.persistence.repository.vaccination.VaccinationRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/vaccinations")
 public class VaccinationController {
 
+    private final VaccinationRepository vaccinationRepository;
+
+    public VaccinationController(VaccinationRepository vaccinationRepository) {
+        this.vaccinationRepository = vaccinationRepository;
+    }
+
     @GetMapping("/all")
     public String redirectToAllVaccinationsPage(Model model) {
+        List<Vaccination> vaccination = vaccinationRepository.findAll();
+        model.addAttribute("vaccinations", vaccinationRepository.findAll());
         return "pages/vaccination/all_vaccination";
     }
 }
