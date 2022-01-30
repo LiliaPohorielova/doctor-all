30322a95642b45794b4f0b3e4570f9c3d19c97a7
Liliia <liliia.pohorielova@nure.ua>
2022-01-30 23:49:26 +0200

    Clean Up Code

--- doctor-all-main/src/main/java/ua/com/alevel/ClinicApplication.java
+++ doctor-all-main/src/main/java/ua/com/alevel/ClinicApplication.java
@@ -5,18 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import ua.com.alevel.elastic.index.DoctorIndex;
 import ua.com.alevel.elastic.index.PatientIndex;
-import ua.com.alevel.persistence.entity.doctor.Doctor;
-import ua.com.alevel.persistence.repository.doctor.DoctorRepository;
-import ua.com.alevel.persistence.repository.user.AdminUserRepository;
-import ua.com.alevel.persistence.repository.user.DoctorUserRepository;
-import ua.com.alevel.persistence.repository.user.PatientUserRepository;
-import ua.com.alevel.persistence.repository.vaccination.VaccinationRepository;
 
 import javax.annotation.PreDestroy;
 
@@ -26,24 +17,9 @@ import javax.annotation.PreDestroy;
         HibernateJpaAutoConfiguration.class })
 public class ClinicApplication {
 
-    private final BCryptPasswordEncoder encoder;
-    private final AdminUserRepository adminUserRepository;
-    private final DoctorUserRepository doctorUserRepository;
-    private final DoctorRepository doctorRepository;
-    private final PatientUserRepository patientUserRepository;
-    private final VaccinationRepository vaccinationRepository;
     private final ElasticsearchOperations elasticsearchOperations;
 
-    public ClinicApplication(BCryptPasswordEncoder encoder,
-                             AdminUserRepository adminUserRepository,
-                             DoctorUserRepository doctorUserRepository,
-                             DoctorRepository doctorRepository, PatientUserRepository patientUserRepository, VaccinationRepository vaccinationRepository, ElasticsearchOperations elasticsearchOperations) {
-        this.encoder = encoder;
-        this.adminUserRepository = adminUserRepository;
-        this.doctorUserRepository = doctorUserRepository;
-        this.doctorRepository = doctorRepository;
-        this.patientUserRepository = patientUserRepository;
-        this.vaccinationRepository = vaccinationRepository;
+    public ClinicApplication(ElasticsearchOperations elasticsearchOperations) {
         this.elasticsearchOperations = elasticsearchOperations;
     }
 
@@ -52,41 +28,8 @@ public class ClinicApplication {
     }
 
     @PreDestroy
-    public void deleteIndex()
-    {
+    public void deleteIndex() {
         elasticsearchOperations.indexOps(DoctorIndex.class).delete();
         elasticsearchOperations.indexOps(PatientIndex.class).delete();
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void listen() {
-/*        Vaccination astrazeneca = new Vaccination();
-        vaccinationRepository.save(astrazeneca);
-
-        Vaccination pfizer = new Vaccination();
-        vaccinationRepository.save(pfizer);
-
-        Vaccination coronaVac = new Vaccination();
-        vaccinationRepository.save(coronaVac);*/
-/*        List<Doctor> doctors = doctorRepository.findAll();
-        for (Doctor doctor:
-             doctors) {
-            System.out.println(doctor);
-        }*/
-
-/*        PatientUser patient = new PatientUser();
-        patient.setEmail("patient@mail.com");
-        patient.setPassword(encoder.encode("rootroot"));
-        patientUserRepository.save(patient);*/
-
-/*        Admin admin = new Admin();
-        admin.setEmail("admin@mail.com");
-        admin.setPassword(encoder.encode("rootroot"));
-        adminUserRepository.save(admin);*/
-
-/*        DoctorUser doctor = new DoctorUser();
-        doctor.setEmail("doctor@mail.com");
-        doctor.setPassword(encoder.encode("rootroot"));
-        doctorRepository.save(doctor);*/
     }
 }
--- vaccination-supplier/src/main/java/ua/com/alevel/VaccinationSupplierApplication.java
+++ vaccination-supplier/src/main/java/ua/com/alevel/VaccinationSupplierApplication.java
@@ -2,48 +2,11 @@ package ua.com.alevel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import ua.com.alevel.entity.Vaccination;
-import ua.com.alevel.repository.VaccinationRepository;
 
 @SpringBootApplication
 public class VaccinationSupplierApplication {
 
-    private final VaccinationRepository vaccinationRepository;
-
-    public VaccinationSupplierApplication(VaccinationRepository vaccinationRepository) {
-        this.vaccinationRepository = vaccinationRepository;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(VaccinationSupplierApplication.class, args);
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void init() {
-/*        Vaccination astrazeneca = new Vaccination();
-        astrazeneca.setName("AstraZeneca");
-        astrazeneca.setImageUrl("https://khoda.gov.ua/image/catalog/Anton/1205vaktsinats%D1%96ya.jpg");
-        astrazeneca.setManufacturer("Great Britain");
-        astrazeneca.setMethodOfAdministration("intramuscularly");
-        astrazeneca.setQuantity(100);
-        vaccinationRepository.save(astrazeneca);
-
-        Vaccination pfizer = new Vaccination();
-        pfizer.setName("Pfizer");
-        pfizer.setImageUrl("https://gdb.voanews.com/3E8439CB-9EF0-48FF-90B2-A50E5CEE7760_w1080_h608.jpg");
-        pfizer.setManufacturer("USA");
-        pfizer.setMethodOfAdministration("intramuscularly");
-        pfizer.setQuantity(200);
-        vaccinationRepository.save(pfizer);
-
-        Vaccination coronaVac = new Vaccination();
-        coronaVac.setName("CoronaVac");
-        coronaVac.setImageUrl("https://media.slovoidilo.ua/media/publications/14/135463/135463-1_large.jpg");
-        coronaVac.setManufacturer("China");
-        coronaVac.setMethodOfAdministration("intramuscularly");
-        coronaVac.setQuantity(300);
-        vaccinationRepository.save(coronaVac);*/
     }
 }
